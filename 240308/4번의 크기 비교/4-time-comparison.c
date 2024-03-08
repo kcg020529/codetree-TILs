7b334f1841ab54220883029e18b3c601c146cb9f
hoxshi <43853585+kcg020529@users.noreply.github.com>
2024-03-08 12:55:34 +0900

    Title: 4번의 크기 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240308/4번의 크기 비교/4-time-comparison.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d, e;
+    scanf("%d",&a);
+    scanf("%d %d %d %d", &b, &c, &d, &e);
+
+    
+    printf("%d\n", (a>b) ? 1:0);
+    printf("%d\n", (a>c) ? 1:0);
+    printf("%d\n", (a>d) ? 1:0);
+    printf("%d", (a>e) ? 1:0);
+
+    return 0;
+}
