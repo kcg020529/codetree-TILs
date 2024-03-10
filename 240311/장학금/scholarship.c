5d6345c6093886a0fb6130228bb19ede1ecd11cd
hoxshi <43853585+kcg020529@users.noreply.github.com>
2024-03-11 08:27:50 +0900

    Title: 장학금, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240311/장학금/scholarship.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if(a>=90) {
+        if(b>=95)
+            printf("100000");
+        else if (b>=90)
+            printf("50000");
+        else
+            printf("0");
+    }
+    else
+        printf("0");
+
+    return 0;
+}
