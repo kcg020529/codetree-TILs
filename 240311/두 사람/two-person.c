87be01d7e067c02010514674e35cd0e142853964
hoxshi <43853585+kcg020529@users.noreply.github.com>
2024-03-11 08:58:21 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240311/두 사람/two-person.c
+++ 240311/두 사람/two-person.c
@@ -8,7 +8,7 @@ int main() {
     scanf("%d %c", &a, &c);
     scanf("%d %c", &b, &d);
 
-    if ((a>18 && c=='M') || (b>18 || d=='M'))
+    if ((a>18 && c=='M') || (b>18 && d=='M'))
         printf("1");
     else
         printf("0");
