c7756d84de4fffebae5339561305b9f2ba5c1f2c
hoxshi <43853585+kcg020529@users.noreply.github.com>
2024-03-15 19:27:24 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240315/a 부터 b 까지/a-to-b.c
+++ 240315/a 부터 b 까지/a-to-b.c
@@ -5,16 +5,12 @@ int main() {
     int a, b;
     scanf("%d %d", &a, &b);
 
-    printf("%d ", a);
-    for(int i=a;i<b;){
-        if(i%2==1){
-            i*=2;
-            printf("%d ", i);
-        }
-        else{
-            i+=3;
-            printf("%d ", i);
-        }
+    while(a<b){
+        printf("%d ", a);
+        if(a%2==1)
+            a*=2;
+        else
+            a+=3;
     }
     return 0;
 }
