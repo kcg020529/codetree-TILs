3f1e0ca36be92ddd8e4527638395ae84ec651a26
hoxshi <43853585+kcg020529@users.noreply.github.com>
2024-03-19 13:23:07 +0900

    Title: 완전수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240319/완전수 판별기/perfect-number-discriminator.c
+++ 240319/완전수 판별기/perfect-number-discriminator.c
@@ -5,15 +5,14 @@ int main() {
     int n, sum= 0;
     scanf("%d", &n);
 
-    for(int i = 1; i<=n;i++){
+    for(int i = 1; i<n;i++){
         if(n%i==0)
             sum+=i;
 
     }
+    
 
-    (sum == n) ? printf("N") : printf("P");
-
-
+    (sum == n) ? printf("P") : printf("N");
 
     return 0;
 }
