73b762a52089629a38edb8f51237609706fbc890
hoxshi <43853585+kcg020529@users.noreply.github.com>
2024-03-22 08:55:17 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240322/1까지 나누기/divide-by-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, i=1;
+    scanf("%d", &n);
+
+    while(n>=0){
+        n/=i;
+        if(n<=0){
+            printf("%d", i);
+            break;
+        }
+        i++;
+    }
+
+    return 0;
+}
