e3d31637958803e33a5fa07af90267bb9d4396ab
hoxshi <43853585+kcg020529@users.noreply.github.com>
2024-03-22 12:59:56 +0900

    Title: 1까지 나누기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240322/1까지 나누기/divide-by-1.c
+++ 240322/1까지 나누기/divide-by-1.c
@@ -5,14 +5,13 @@ int main() {
     int n, i=1;
     scanf("%d", &n);
 
-    while(n>=0){
+    while(n>=1){
         n/=i;
-        if(n<=0){
+        if(n<=1){
             printf("%d", i);
             break;
         }
-        else
-            i++;
+        i++;
     }
 
     return 0;
