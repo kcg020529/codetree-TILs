268bd40f3b748b59b5ca10352268f05c8c6a28a5
hoxshi <43853585+kcg020529@users.noreply.github.com>
2024-03-26 11:59:10 +0900

    Title: 소수 판별기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240326/소수 판별기/prime-number-discriminator.c
+++ 240326/소수 판별기/prime-number-discriminator.c
@@ -2,22 +2,22 @@
 #include <stdbool.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
+	// 변수 선언 및 입력
+	int n;
+	scanf("%d", &n);
+	bool satisfied = true;
+	
+	for(int i = 2; i < n; i++) {
+		// 1과 자기자신을 제외한 약수가 전혀 없다면 그 수는 소수입니다.
+		if(n % i == 0)
+			satisfied = false;
+	}
 
-    bool satisfied = true;
-
-    for(int i = 2; i < n; i++) {
-        if(n%i==0){
-            satisfied = false;
-            break;
-        }
-    }
-    //(satisfied = false) ? printf("C") : printf("P");
-    if(satisfied = false)
-        printf("C");
-    else
-        printf("P");
-
+	//출력
+	if(satisfied == true)
+		printf("P");
+	else
+		printf("C");
+	
     return 0;
 }
