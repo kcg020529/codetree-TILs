81421db747b98079c2e7bb7ed441af08396adccc
hoxshi <43853585+kcg020529@users.noreply.github.com>
2024-03-26 11:52:12 +0900

    Title: 소수 판별기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240326/소수 판별기/prime-number-discriminator.c
+++ 240326/소수 판별기/prime-number-discriminator.c
@@ -10,6 +10,7 @@ int main() {
     for(int i = 2; i < n; i++) {
         if(n%i==0)
             satisfied = false;
+            break;
     }
     (satisfied = false) ? printf("C") : printf("P");
     
