3c1d5736045fce99234e65d3105d9c1e9a163f99
hoxshi <43853585+kcg020529@users.noreply.github.com>
2024-04-08 19:37:59 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/원소 10개의 합/sum-of-10-elements.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int a[10];
+    int sum = 0;
+
+    for(int i = 0; i<10; i++){
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+    printf("%d", sum);
+    return 0;
+}
