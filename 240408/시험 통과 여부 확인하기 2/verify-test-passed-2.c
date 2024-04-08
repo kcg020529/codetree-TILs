58dea5777c75660909d587bb3efc361a5932d54c
hoxshi <43853585+kcg020529@users.noreply.github.com>
2024-04-08 20:46:26 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/시험 통과 여부 확인하기 2/verify-test-passed-2.c
+++ 240408/시험 통과 여부 확인하기 2/verify-test-passed-2.c
@@ -1,30 +1,42 @@
 #include <stdio.h>
 
 int main() {
-
-    int sum, n, cnt = 0, a[3];
-    double avg;
-    
+    // 학생 수 n 입력받기
+    int n;
     scanf("%d", &n);
 
-    for(int i = 0; i < n; i++){
-        sum = 0;
-        avg = 0;
+    // 통과한 사람의 수를 나타내는 변수 : pass_people
+    int pass_people = 0;
 
-        for(int j = 0; j<3; j++){
-            scanf("%d", &a[j]);
-            sum+=a[j];
+    for(int i = 0; i < n; i++) {
+        // 정수 배열 선언
+        int arr[4];
+        int sum = 0;
+
+        // 4개의 정수를 배열에 입력받아 저장합니다.
+        for(int j = 0; j < 4; j++) {
+            scanf("%d", &arr[j]);
         }
 
-        avg = sum / 4;
-        if(avg>=60){
+        // 4과목의 점수의 합을 구합니다.
+        for(int j = 0; j < 4; j++) {
+            sum += arr[j];
+        }
+		
+        // 평균을 구합니다.
+        double avg = (double)sum / 4;
+
+        //출력
+        if(avg >= 60) {
             printf("pass\n");
-            cnt+=1;
+            pass_people++;
         }
         else
             printf("fail\n");
     }
-    printf("%d", cnt);
+
+    // 통과한 사람의 수 출력
+    printf("%d", pass_people);
 
     return 0;
 }
