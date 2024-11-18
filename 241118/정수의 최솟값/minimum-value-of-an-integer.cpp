5ad5dd14caff938f8593296742f216aad7b2af3f
hoxshi <43853585+kcg020529@users.noreply.github.com>
2024-11-18 17:12:18 +0900

    Title: 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241118/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int SmallestNum(int a, int b, int c);
+
+int main() {
+    
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int result = SmallestNum(a, b, c);
+    cout << result << endl;
+
+    return 0;
+}
+
+int SmallestNum(int a, int b, int c)
+{
+    int min_val = 0;
+    if(a > b && c > b) min_val = b;
+    else if(a > b && b > c) min_val = c;
+    else min_val = a;
+
+    return min_val;
+}
