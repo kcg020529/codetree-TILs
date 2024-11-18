b17e56939d570d98e8e98b394715e0d72a08b262
hoxshi <43853585+kcg020529@users.noreply.github.com>
2024-11-18 17:13:10 +0900

    Title: 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241118/정수의 최솟값/minimum-value-of-an-integer.cpp
+++ 241118/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -17,8 +17,8 @@ int main() {
 int SmallestNum(int a, int b, int c)
 {
     int min_val = 0;
-    if(a > b && c > b) min_val = b;
-    else if(a > b && b > c) min_val = c;
+    if(a >= b && c >= b) min_val = b;
+    else if(a >= b && b >= c) min_val = c;
     else min_val = a;
 
     return min_val;
